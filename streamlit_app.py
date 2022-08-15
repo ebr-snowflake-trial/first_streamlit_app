3560d04add30427a6f6e67c8a74bbb12152bf425
Eric Richardson <eric.richardson.snowflake.trial@gmail.com>
2022-08-15 15:33:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
-import streamlit
 import pandas
+import streamlit
+import requests
+
 
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -19,10 +21,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit = "kiwi"
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit)
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
+
 # write your own comment -what does the next line do?
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
