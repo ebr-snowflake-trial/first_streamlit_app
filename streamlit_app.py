326942e9008169ba06bb359f1083ffee4cfcf38c
Eric Richardson <eric.richardson.snowflake.trial@gmail.com>
2022-08-15 18:21:13 -0400

    Updated Requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import pandas
 import streamlit
 import requests
+import snowflake.connector
 
 
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
